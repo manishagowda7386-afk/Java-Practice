17a9ea1ef9337f3f78f7a6240eb8ff06a5179131
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-11 17:53:33 +0530

    Add SmallestElement program

---
+++ Arrays/SmallestElement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr=new int[5];
+
+        for(int i=0;i<5;i++)
+            arr[i]=sc.nextInt();
+
+        int min=arr[0];
+
+        for(int i=1;i<5;i++)
+            if(arr[i]<min)
+                min=arr[i];
+
+        System.out.println("Smallest = "+min);
+
+        sc.close();
+    }
+}
